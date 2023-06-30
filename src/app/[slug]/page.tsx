56ac675ad3ecf3b638de435a469a974a1a00d4cf
Anthony Du <github@anthonydu.com>
2023-06-30 23:39:13 +0800

    Markdown Rendering and Dynamic Routing Implemented

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import matter from "gray-matter";
+import ReactMarkdown from "react-markdown";
+
+export default function Page({ params }: {params: { slug: string}}) {
+  const content = fs.readFileSync(`public/posts/${params.slug}.md`, 'utf8');
+  const { data: frontmatter, content: markdownBody } = matter(content);
+
+  return (
+    <ReactMarkdown className="markdown">{markdownBody}</ReactMarkdown>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " bg-slate-800 text-slate-200"}>
+        <header className="fixed top-0 w-full h-16 flex items-center pl-5 backdrop-blur border-b border-slate-700 text-2xl">
+          <a href="/">Anthony Du's Blog</a>
+        </header>
+        <main className="pt-36 container mx-auto">
+          <div className="mx-10">
+          {children}
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
