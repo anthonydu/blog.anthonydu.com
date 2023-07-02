65f4e01000171c49623744327539afde2577971b
Anthony Du <github@anthonydu.com>
2023-07-02 12:39:21 +0800

    Added Hotjar, optimized scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,17 +25,33 @@ export default function RootLayout({
         </main>
 
         {/* Default Statcounter code for Anthony Du's Blog https://blog.anthonydu.com */}
-        <script type="text/javascript">
-          var sc_project=12896567; 
-          var sc_invisible=1; 
-          var sc_security=`a85b9a10`; 
-        </script>
-        <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js"async></script>
+        <Script id="statcounter">
+          {`
+            var sc_project=12896567; 
+            var sc_invisible=1; 
+            var sc_security="a85b9a10"; 
+          `}
+        </Script>
+        <Script src="https://www.statcounter.com/counter/counter.js" async></Script>
         {/* End of Statcounter Code */}
 
-        <script async data-id="101416538" src="//static.getclicky.com/js"></script>
-        <script data-goatcounter="https://anthonydu.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
-        <script defer src="https://service.watchthem.live/pixel/ssaLU3aUddUUaaUa"></script>
+        {/* Hotjar Tracking Code for https://blog.anthonydu.com */}
+        <Script id="hotjar">
+          {`
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3557110,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
+
+        <Script async data-id="101416538" src="//static.getclicky.com/js"></Script>
+        <Script data-goatcounter="https://anthonydu.goatcounter.com/count" async src="//gc.zgo.at/count.js"></Script>
+        <Script defer src="https://service.watchthem.live/pixel/ssaLU3aUddUUaaUa"></Script>
       </body>
     </html>
   )
